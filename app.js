a30e586ea4e86a942baf3536573cb4bc499b6079
Allan Katongole <kapsonkatongole@gmail.com>
2019-09-23 18:05:45 +0300

    Add post and get soutes

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+//Import Routes
+const postsRoute = require('./routes/posts');
 
 //middleware
+app.use(express.json())
+app.use('/posts', postsRoute);
+
 app.use('/posts', () => {
     console.log('This is the middlewarre running');
 })
+
+mongoose.connect(process.env.DB_CONNECTION,
+ {useNewUrlParser: true},
+ () => console.log('Connected to DB'))
 
 //ROUTES
 app.get('/' , (req, res) => {
     res.send('We are on home');
 });
-
-app.get('/posts' , (req, res) => {
-    res.send('We are on posts');
-});
-
 
 app.listen(3000)
